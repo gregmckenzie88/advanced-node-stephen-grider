dc7652f40bb259d9522179f850f784a173446c2f
Greg Mckenzie <gregmckenzie@Gregs-iMac.local>
2019-06-16 21:34:33 -0400

    caching with redis

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://greg:password1@ds145574.mlab.com:45574/blog_dev',
   cookieKey: '123123123'
 };
